946ec800c01002ab7e4648d2e63acf09e556dd3a
un <jack@unsimp.tech>
2022-01-01 18:28:05 +0000

    add my projects

---
+++ basics.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // says hello to the ball licker
+    cout << "hello ball licker \n";
+    // says line2
+    cout << "line2 \n";
+    // defines a interger and prints it to sdout
+    int mynum = 69420;
+    mynum = 15;
+    cout << mynum;
+    cout << "\n";
+
+
+    int mygun = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals) w3 schools says to use doubles insted of floats for most calcs
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (text)
+    bool myBoolean = true;       // Boolean (true or false) 
+    cout << "i am " << mygun << " years old. \n";
+
+    // operations in vars
+    int m = 5;
+    int v = 6;
+    int sum= v+m;
+    cout << sum;
+
+    // vars can also be listed in one line
+    int x = 5, y = 6, z = 50;
+    cout << x + y + z;
+    // append "const" to the start on a variable to make it Read-Only
+    const int number = 25; // now unwriteable
+    // number = 10; will cause a compile error
+
+    // accept user input with "cin" 
+    // cin "see in" cout "see out"
+    int numberInput;
+    cout << "Type a number: "; // prints "see out"
+    cin >> numberInput; // waits for input
+    cout << "your number is " << numberInput << "\n";
+
+    //use "e" in a double or float to indicate a power of
+    float f1 = 35e3;
+    double d1 = 12E4;
+    cout << f1;
+    cout << d1; 
+    cout << "\n";
+    // use char to define a single character. acsii can also be used
+    char myGrade = 'B';
+    cout << myGrade;
+    char ascii = 65;
+    cout << ascii;
+    return 0;
+}
---
+++ message.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string link = "https://www.unsimp.tech/8b68df544106a3e40bb82077c32b26625f4ea55f.png";
+    bool criminal = false;
+    cout << "NAME: ";
+    cout << "Un (Jane Dez)";
+    cout << "\n";
+    cout << "SEX (Gender): ";
+    cout << "Transfemme";
+    cout << "\n";
+    cout << "Short Description of Character and their goals: ";
+    cout << "\n Jane is a queer c++ enthusiast and wants to become a government leader along side bama. end goal is the cleansing";
+    cout << "\n Link to the Minecraft Skin you'd like to use on the server: ";
+    cout << link;
+    cout << "\n Will you be roleplaying as a criminal?: " << criminal;
+    
+
+}
